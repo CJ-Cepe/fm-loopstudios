951f5b026034411bce3082d64c18bc426317a84b
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2024-07-10 17:20:08 +0800

    Refactor: set inert attribute on main and footer when slide-out menu is active and adjust menu dimensions

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const hamBtn = document.querySelector('.hamburger');
-const navList = document.querySelector('.header__nav-list');
 const media = window.matchMedia('(width <= 43.75rem)');
 const body = document.querySelector('body');
+const main = document.querySelector('main');
+const footer = document.querySelector('footer');
 
 media.addEventListener('change', (e) => {
     if (!e.matches) {
         hamBtn.setAttribute('aria-expanded', false);
+        body.classList.remove('overflow-hidden');
+        main.removeAttribute('inert');
+        footer.removeAttribute('inert');
     }
 })
 
@@ -13,4 +17,14 @@ hamBtn.addEventListener('click', () => {
     const btnState = hamBtn.getAttribute('aria-expanded') === 'true'
     hamBtn.setAttribute('aria-expanded', !btnState);
     hamBtn.focus();
+
+    if (!btnState) {
+        body.classList.add('overflow-hidden');
+        main.setAttribute('inert', '');
+        footer.setAttribute('inert', '');
+    } else {
+        body.classList.remove('overflow-hidden');
+        main.removeAttribute('inert');
+        footer.removeAttribute('inert');
+    }
 })
